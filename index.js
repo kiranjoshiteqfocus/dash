fbbf7d9ba1fe6b6490ff24fcb4791ae4cb0f6339
kiranjoshiteqfocus <124032440+kiranjoshiteqfocus@users.noreply.github.com>
2023-03-10 14:40:22 +0530

    Added express

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const { MongoClient } = require("mongodb");
+const  express = require("express");
+const cors = require("cors");
 
 const uri = "mongodb+srv://kiranjoshi:v51n8BEu1PwchVVc@cluster0.xtnuwk8.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-async function run(){
-    try{
-        const database = client.db('sample_dashboard');
-        const dash = database.collection('dash_data');
+const database = client.db('sample_dashboard');
 
-        const query = {ids: 1};
-        const data = await dash.findOne(query);
+app.get('/projectcards', async (req, res) => {
+    const dash = database.collection('dash_data');
+    const query = {ids: 1};
+    const data = await dash.findOne(query);
+    return res.json(data);
+})
 
-        console.log(data.projects);
-    }finally{
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
+app.listen(8800, async ()=>{
+    console.log("backend connected")
+})
